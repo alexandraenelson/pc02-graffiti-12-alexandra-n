b622290f5ba4ce9524d7760a39d7f66da2e5953c
benna-mallory <69868279+benna-mallory@users.noreply.github.com>
2020-09-04 12:20:27 -0600

    Test Upload

---
+++ PC02-Demo.py
@@ -0,0 +1,37 @@
+
+'''
+Turtle starter code
+ATLS 1300
+Author: Dr. Z
+Author: Mallory Benna
+May 29, 2020
+'''
+
+from turtle import * #import the library of commands that you'd like to use
+
+colormode(255)
+
+# Create a panel to draw on. 
+screen = Screen()
+w = 750 # width of panel
+h = 750 # height of panel
+screen.setup(width=w, height=h) #600 x 600 is a decent size to work on. 
+#You can experiment by making it the size of your screen or super tiny!
+
+# Create a colorful background and add Bezos image to it
+image = "Bezos.gif"
+screen.bgcolor("lightsteelblue1")
+screen.bgpic(image)
+
+#=======Add your code here======
+
+
+#Creating turtle
+t = Turtle()
+t.pensize(10)
+
+
+
+
+
+
